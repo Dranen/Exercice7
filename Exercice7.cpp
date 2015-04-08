8bee8cfe20dfa0156b2eaee695b9d0b678cde269
Dranen <mathieu.debonydelavergne@epfl.ch>
2015-04-08 17:00:25 +0200

    Optimisation des facteurs constant dans le temps

--- Exercice7.cpp
+++ Exercice7.cpp
@@ -19,7 +19,7 @@ int main()
   double xL, xR, dx, u2_max=0, tfinal = 0, dt, leftboundaryvalue = 0, rightboundaryvalue = 0;
   double A=1., omega=0., omega_start, omega_stop, delta_omega = 0.0, t, energy, maxenergy;
   u_squared u2;
-  std::vector<double> *fpast, *fnow, *fnext, *coeff, *temp;
+  std::vector<double> *fpast, *fnow, *fnext, *coeff, *temp, *u, *u_2, *beta;
   boundary_condition left_bc, right_bc;
 
   // open output file streams
@@ -50,6 +50,9 @@ int main()
   fnow = new vector<double>(Npos);
   fnext = new vector<double>(Npos);
   coeff = new vector<double>(Npos);
+  u = new vector<double>(Npos);
+  u_2 = new vector<double>(Npos);
+  beta = new vector<double>(Npos);
   
   // coeff is beta^2
   for(int ip = 0; ip < Npos; ++ip)
@@ -60,6 +63,13 @@ int main()
 
   contexte_scan_frequence(nscan, omega, omega_start, omega_stop, delta_omega);
 
+  //calcul des vitesse en fonctions des positions et calcul du parametre beta
+  for(int ip = 0; ip < Npos; ++ip)
+  {
+      (*u_2)[ip] = u2(xL + dx * ip);
+      (*u)[ip] = sqrt((*u_2[ip]));
+      (*beta)[ip] = (*u)[ip]*dt/dx;
+  }
 
 
   //Sortie sur la vitesse en fonction de la position ------
@@ -123,7 +133,7 @@ int main()
             {
                 for(int ip = 1; ip < (Npos - 1); ++ip)
                 {
-                    (*fnext)[i] = (dt*dt*((0.5*(sqrt(u2(x[i])))*(sqrt(u2(x[i+1]))-sqrt(u2(x[i-1])))*((*fnow)[i+1]-(*fnow)[i-1])) + (u2(x[i])*((*fnow)[i+1]-2*(*fnow)[i]+(*fnow)[i-1])))/(dx*dx)) + 2*(*fnow)[i] - (*fpast)[i];
+                    (*fnext)[i] = 0.5*(*beta)[i]*((*u)[i+1]-(*u)[i-1])*((*fnow)[i+1]-(*fnow)[i-1])*dt/dx + (*beta)[i]*(*beta)[i]*((*fnow)[i+1]-2*(*fnow)[i]+(*fnow)[i-1]) + 2*(*fnow)[i] - (*fpast)[i];
                 }
             }
 
@@ -131,9 +141,9 @@ int main()
             // apply boundary conditions
             //
             // left boundary (at xL)
-            calcul_condition_bord(left_bc, leftboundaryvalue, A, omega, t, fnow, fnext, 0);
+            calcul_condition_bord(left_bc, leftboundaryvalue, A, omega, t, fnow, fnext, 0, (*beta)[i]);
             // right boundary (at xr)
-            calcul_condition_bord(right_bc, rightboundaryvalue, A, omega, t, fnow, fnext, Ninter);
+            calcul_condition_bord(right_bc, rightboundaryvalue, A, omega, t, fnow, fnext, Ninter, (*beta)[i]);
 
             t += dt;
 
@@ -165,6 +175,9 @@ int main()
   delete fnow;
   delete fnext;
   delete coeff;
+  delete u;
+  delete u_2;
+  delete beta;
   
   w_ofs.close();
   energy_ofs.close();
