6f846be2ce5c24ef9959eec3a26fd64df65529cf
Dranen <mathieu.debonydelavergne@epfl.ch>
2015-04-15 10:03:21 +0200

    Ajout de graphique

--- Analyse_donne.m
+++ Analyse_donne.m
@@ -2,18 +2,19 @@ function Analyse_donne(nom)
 
 donnee = lecture_fichier(nom);
 
-x =  donnee[1];
-u = donnee[2];
-t = donnee[3];
-f = donnee[4];
-energy = donnee[5];
-omega = donnee[6];
-maxenergy = donnee[7];
+x =  donnee(1);
+u = donnee(2);
+t = donnee(3);
+f = donnee(4);
+energy = donnee(5);
+omega = donnee(6);
+maxenergy = donnee(7);
 
 Graphique_u(x,u);
 Graphique_maxf(x,f);
 Graphique_xtf(x,t,f);
 Graphique_xtf3D(x,t,f);
 Graphique_omega(omega, maxenergy);
+Graphique_xtE(x,t,energy);
 
 end
