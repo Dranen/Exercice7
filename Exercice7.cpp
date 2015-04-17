de8c27cd083452f769c74499eedadb66cf7fed20
Dranen <mathieu.debonydelavergne@epfl.ch>
2015-04-17 17:12:48 +0200

    Modification du programme pour y ajouter des calculs de convergences

--- Exercice7.cpp
+++ Exercice7.cpp
@@ -17,13 +17,14 @@ using namespace std;
 //
 int main() 
 { 
-  int Ninter,eqref, ucase, Npos, nscan;
-  double xL, xR, dx, u2_max=0, tfinal = 0, dt, leftboundaryvalue = 0, rightboundaryvalue = 0;
-  double A=1., omega_start=0., omega_stop, delta_omega = 0.0, t, energy, maxenergy;
+  int eqref, ucase, Ninter_start, Ninter_stop, nscan;
+  double xL, xR, u2_max=0, tfinal = 0, leftboundaryvalue = 0, rightboundaryvalue = 0;
+  double A=1., omega_start=0., omega_stop, CFL_start, CFL_stop, dx_stop;
   u_squared u2;
-  std::vector<double> *fpast = NULL, *fnow = NULL, *fnext = NULL, *coeff = NULL, *temp = NULL, *u_1 = NULL, *u_2 = NULL, *beta = NULL;
+  std::vector<double>  *coeff = NULL, *u_1 = NULL, *beta = NULL;
   boundary_condition left_bc, right_bc;
   std::string nom, provisoire;
+  mode choix;
 
   //initialisation de parametres généraux
   contexte_general(nom, Ninter, xL, xR, eqref, ucase);
@@ -49,167 +50,193 @@ int main()
 
   //initialisation des différentes valeurs
 
-  Npos = Ninter+1;
-  dx=(xR - xL) / Ninter;
+  contexte_vitesse(ucase, xL, xR, u2);
+  contexte_temporelle(tfinal, CFL_start);
 
-  contexte_vitesse(ucase, xL, xR, dx, Npos, u2_max, u2);
-  contexte_temporelle(dt, tfinal, dx, u2_max);
-
-  coeff = new vector<double>(Npos);
-  u_1 = new vector<double>(Npos);
-  u_2 = new vector<double>(Npos);
-  beta = new vector<double>(Npos);
-
-  
   contexte_bord("left", left_bc, leftboundaryvalue, A, omega_start);
   contexte_bord("right", right_bc, rightboundaryvalue, A, omega_start);
 
-  contexte_scan_frequence(nscan, omega_stop);
-  vector<double> omega(nscan);
-  if(nscan > 1)
-  {
-      for(int jscan = 0; jscan < nscan; ++jscan)
-      {
-         omega[jscan] = omega_start + static_cast<double>(jscan)*((omega_stop-omega_start)/static_cast<double>(nscan-1));
-      }
-  }
-  else
-  {
-      omega[0] = omega_start;
-  }
+  contexte_mode(nscan, omega_stop, dx_stop, CFL_stop, choix);
 
-
-  //calcul des vitesse en fonctions des positions et calcul du parametre beta
-  for(int ip = 0; ip < Npos; ++ip)
-  {
-      (*u_2)[ip] = u2(xL + dx * ip);
-      (*u_1)[ip] = sqrt(((*u_2)[ip]));
-      (*beta)[ip] = (*u_1)[ip]*dt/dx;
-      // coeff is beta^2
-      (*coeff)[ip] = (*beta)[ip] * (*beta)[ip];
-  }
-
+   if(choix != convergence_Ninter)
+   {
+       Ninter_stop = Ninter_start;
+   }
+   if(choix != convergence_CFL)
+   {
+       CFL_stop = CFL_start;
+   }
+   if(choix != convergence_Ninter)
+   {
+       omega_stop = omega_start;
+   }
 
   //Sortie sur la vitesse en fonction de la position ------
 
-  double pos;
-  for(int ip = 0; ip < Npos; ++ip)
+    double pos, u_2 = 0;
+    for(int ip = 0; ip < Ninter_stop+1; ++ip)
     {
-      pos = xL + dx * ip;
-      p_u_ofs << pos << " " << u2(pos) << std::endl;
+        pos = xL + (xR - xL) / (Ninter_stop+1) * ip;
+        u_2 = u2(pos);
+        p_u_ofs << pos << " " << u_2 << endl;
+        if(u_2 > u2_max)
+        {
+            u2_max = u_2;
+        }
     }
-  p_u_ofs.close();
+    cout << "u2_max="<<u2_max << endl;
+    p_u_ofs.close();
   
+    if(choix == unique)
+    {
+        int Npos = Ninter_start+1;
+        double dx =(xR - xL) / Ninter_start;
+        double dt = CFL_start * dx / sqrt(u2_max);
+
+        cout << "dt=" << dt << endl;
+
+        coeff = new vector<double>(Npos);
+        u_1 = new vector<double>(Npos);
+        beta = new vector<double>(Npos);
+
+        //calcul des vitesse en fonctions des positions et calcul du parametre beta
+        for(int ip = 0; ip < Npos; ++ip)
+        {
+            (*u_1)[ip] = sqrt((u2(xL + dx * ip)));
+            (*beta)[ip] = (*u_1)[ip]*dt/dx;
+            // coeff is beta^2
+            (*coeff)[ip] = (*beta)[ip] * (*beta)[ip];
+        }
+
+        simulation(u_1,beta,coeff,dx,dt,eqref,ucase,Npos,tfinal,omega_start,A,left_bc,leftboundaryvalue,right_bc,rightboundaryvalue,choix,w_ofs,energy_ofs,maxenergy_ofs,false);
+
+        delete coeff;
+        delete u_1;
+        delete beta;
+    }
+    else if(choix == frequence)
+    {
+        int Npos = Ninter_start+1;
+        double dx =(xR - xL) / Ninter_start;
+        double dt = CFL_start * dx / sqrt(u2_max);
+
+        cout << "dt=" << dt << endl;
+
+        coeff = new vector<double>(Npos);
+        u_1 = new vector<double>(Npos);
+        beta = new vector<double>(Npos);
+
+        //calcul des vitesse en fonctions des positions et calcul du parametre beta
+        for(int ip = 0; ip < Npos; ++ip)
+        {
+            (*u_1)[ip] = sqrt((u2(xL + dx * ip)));
+            (*beta)[ip] = (*u_1)[ip]*dt/dx;
+            // coeff is beta^2
+            (*coeff)[ip] = (*beta)[ip] * (*beta)[ip];
+        }
 
 
-  // frequency scan ------------------------------------------
-  #pragma omp parallel for default(shared) private(energy, maxenergy, temp, fpast, fnow, fnext, t) schedule(dynamic) if(nscan > 1)
-  for(int jscan = 0; jscan < nscan; ++jscan) 
-  {
-      fpast = new vector<double>(Npos);
-      fnow = new vector<double>(Npos);
-      fnext = new vector<double>(Npos);
-      
+        vector<double> omega(nscan);
+        for(int jscan = 0; jscan < nscan; ++jscan)
+        {
+            omega[jscan] = omega_start + static_cast<double>(jscan)*((omega_stop-omega_start)/static_cast<double>(nscan-1));
+        }
 
-       // initialize the first two time slices of f
-       double xo=3e5;
-       double x;
-       for(int ip = 0; ip < Npos; ++ip)
-       {
-          if(ucase==2){
-            x = static_cast<double>(ip)*dx-xo;
-            (*fnow)[ip] = exp(-x*x/2.5e9);
-            (*fpast)[ip]=(*fnow)[ip];
-          }else{
-            (*fpast)[ip] = 0.0;
-            (*fnow)[ip] = 0.0;
-          }
-       }
-
-
-      // output the positions and the phase velocity squared for plotting purposes
-      t = 0;
-      energy = get_energy(*fnow,dx);
-      maxenergy = energy;
-      
-      if(jscan == 0) 
-      {
-        w_ofs << t << " " << *fnow << endl;
-        energy_ofs << t << " " << energy << endl;
-      }
-      
-
-      // time loop --------------------------------
-      chrono::high_resolution_clock::time_point end;
-      chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-      do
-      {
-            //cout << " time evolution: " << t << endl;
-            if(eqref == 1)
+        #pragma omp parallel for default(shared) schedule(dynamic)
+        for(int jscan = 0; jscan < nscan; ++jscan)
+        {
+            #pragma omp critical
             {
-                #pragma omp parallel for if(Npos > 1000 && nscan == 1)
-                for(int ip = 1; ip < (Npos - 1); ++ip)
-                {
-                    (*fnext)[ip]=-(*fpast)[ip]+2*(1-(*coeff)[ip])*(*fnow)[ip]+(*coeff)[ip]*((*fnow)[ip-1]+(*fnow)[ip+1]);
-                }
+                cout << "Scan frequency: " << jscan << endl;
             }
-            else
+            simulation(u_1,beta,coeff,dx,dt,eqref,ucase,Npos,tfinal,omega[jscan],A,left_bc,leftboundaryvalue,right_bc,rightboundaryvalue,choix,w_ofs,energy_ofs,maxenergy_ofs,false);
+        }
+
+        delete coeff;
+        delete u_1;
+        delete beta;
+    }
+    else if(choix == convergence_CFL)
+    {
+        int Npos = Ninter_start+1;
+        double dx = (xR - xL) / Ninter_start;
+
+        coeff = new vector<double>(Npos);
+        u_1 = new vector<double>(Npos);
+        beta = new vector<double>(Npos);
+
+        //calcul des vitesse en fonctions des positions et calcul du parametre beta
+        for(int ip = 0; ip < Npos; ++ip)
+        {
+            (*u_1)[ip] = sqrt((u2(xL + dx * ip)));
+            (*beta)[ip] = (*u_1)[ip]*dt/dx;
+            // coeff is beta^2
+            (*coeff)[ip] = (*beta)[ip] * (*beta)[ip];
+        }
+
+        vector<double> dt(nscan);
+        for(int jscan = 0; jscan < nscan; ++jscan)
+        {
+            dt[jscan] = (CFL_start + static_cast<double>(jscan)*((CFL_stop-CFL_start)/static_cast<double>(nscan-1))) * dx / sqrt(u2_max);
+        }
+
+        #pragma omp parallel for default(shared) schedule(dynamic)
+        for(int jscan = 0; jscan < nscan; ++jscan)
+        {
+            #pragma omp critical
             {
-                #pragma omp parallel for if(Npos > 1000 && nscan == 1)
-                for(int ip = 1; ip < (Npos - 1); ++ip)
-                {
-                    (*fnext)[ip] = 0.5*(*beta)[ip]*((*u_1)[ip+1]-(*u_1)[ip-1])*((*fnow)[ip+1]-(*fnow)[ip-1])*dt/dx + (*beta)[ip]*(*beta)[ip]*((*fnow)[ip+1]-2*(*fnow)[ip]+(*fnow)[ip-1]) + 2*(*fnow)[ip] - (*fpast)[ip];
-                }
+                cout << "dt=" << dt[jscan] << endl;
+            }
+            simulation(u_1,beta,coeff,dx,dt[jscan],eqref,ucase,Npos,tfinal,omega_start,A,left_bc,leftboundaryvalue,right_bc,rightboundaryvalue,choix,w_ofs,energy_ofs,maxenergy_ofs,false);
+        }
+
+        delete coeff;
+        delete u_1;
+        delete beta;
+
+    }
+    else if(choix == convergence_Ninter)
+    {
+        vector<double> dt(nscan);
+        vector<double> dx(nscan);
+        vector<int> Npos(nscan);
+        for(int jscan = 0; jscan < nscan; ++jscan)
+        {
+            Npos[jscan] = (Ninter_start + static_cast<double>(jscan)*((Ninter_stop-Ninter_start)/static_cast<double>(nscan-1))) + 1;
+            dx[jscan] = (xR - xL)/(Npos[jscan]-1);
+            dt[jscan] =  CFL_start * dx[jscan] / sqrt(u2_max);
+        }
+
+        #pragma omp parallel for default(shared) private(coeff, u_1, beta) schedule(dynamic)
+        for(int jscan = 0; jscan < nscan; ++jscan)
+        {
+            coeff = new vector<double>(Npos);
+            u_1 = new vector<double>(Npos);
+            beta = new vector<double>(Npos);
+
+            //calcul des vitesse en fonctions des positions et calcul du parametre beta
+            for(int ip = 0; ip < Npos; ++ip)
+            {
+                (*u_1)[ip] = sqrt((u2(xL + dx * ip)));
+                (*beta)[ip] = (*u_1)[ip]*dt/dx;
+                // coeff is beta^2
+                (*coeff)[ip] = (*beta)[ip] * (*beta)[ip];
             }
 
+            #pragma omp critical
+            {
+                cout << "dt=" << dt[jscan] << endl;
+                cout << "dx=" << dx[jscan] << endl;
+            }
+            simulation(u_1,beta,coeff,dx[jscan],dt[jscan],eqref,ucase,Npos[jscan],tfinal,omega_start,A,left_bc,leftboundaryvalue,right_bc,rightboundaryvalue,choix,w_ofs,energy_ofs,maxenergy_ofs,false);
 
-            // apply boundary conditions
-            //
-            // left boundary (at xL)
-            calcul_condition_bord(true, left_bc, leftboundaryvalue, A, omega[jscan], t, fnow, fnext, 0, (*beta)[0]);
-            // right boundary (at xr)
-            calcul_condition_bord(false, right_bc, rightboundaryvalue, A, omega[jscan], t, fnow, fnext, Npos-1, (*beta)[Npos-1]);
-
-            t += dt;
-
-            temp = fpast;
-            fpast = fnow;
-            fnow  = fnext;
-            fnext = temp;
-
-            double energy = get_energy((*fnow),dx);
-
-            if(energy > maxenergy)
-              maxenergy = energy;
-
-            if(nscan == 1)
-              {
-                w_ofs << t<< " " << *fnow << endl;
-                energy_ofs << t << " " << energy << endl;
-              }
-
-      } while(t < tfinal); // end of time loop -----------------------------
-      end = std::chrono::high_resolution_clock::now();
-      #pragma omp critical
-      {
-          cout << "Scan frequency: " << jscan << endl;
-          cerr << endl << setprecision(std::numeric_limits<double>::digits10 + 1) << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl;
-
-          maxenergy_ofs << omega[jscan] << " " << maxenergy << endl;
-      }
-
-      delete fpast;
-      delete fnow;
-      delete fnext;
-      
-    } // end of frequency scan loop -------------------
+            delete coeff;
+            delete u_1;
+            delete beta;
+        }
+    }
 
   w_ofs.close();
   energy_ofs.close();
   maxenergy_ofs.close();
-
-  delete coeff;
-  delete u_1;
-  delete u_2;
-  delete beta;
 }
--- Exercice7_io.h
+++ Exercice7_io.h
@@ -28,72 +28,59 @@ void contexte_general(std::string& nom ,int& Ninter, double& xL, double& xR, int
 {
     std::cerr << "Nom de la simulation " << std::flush;
     std::cin >> nom;
-    std::cerr << "Number of grid points? " << std::flush;
+    std::cerr << std::endl << "Number of grid points? " << std::flush;
     std::cin >> Ninter;
-    std::cerr << "xL? " << std::flush;
+    std::cerr << std::endl << "xL? " << std::flush;
     std::cin >> xL;
-    std::cerr << "xR? " << std::flush;
+    std::cerr << std::endl << "xR? " << std::flush;
     std::cin >> xR;
-    std::cerr << "Quelle equation (1) ou (2)?"<< std::flush;
+    std::cerr << std::endl << "Quelle equation (1) ou (2) ou (3)?"<< std::flush;
     std::cin >> eqref;
-    std::cerr << "quel question? 7.2 (taper 0) / 7.3 (taper 1) / 7.4 (taper 2):" << std::flush;
+    std::cerr << std::endl << "quel question? 7.2 (taper 0) / 7.3 (taper 1) / 7.4 (taper 2):" << std::flush;
     std::cin >> ucase;
 }
 
-void contexte_vitesse(int const& ucase, double const& xL, double const& xR, double const& dx, int const& Npos, double& u2_max, u_squared& u2)
+void contexte_vitesse(int const& ucase, double const& xL, double const& xR, u_squared& u2)
 {
     double uL, uR, u, hocean = 0, xocean=0, hplage=0;
     switch(ucase){
       case 0:
-        std::cerr << "value of u? " << std::flush;
+        std::cerr << std::endl << "value of u? " << std::flush;
         std::cin >> u;
         u2 = u_squared(u);
-        u2_max = u2(0);
         break;
       case 1:
-        std::cerr << "u_L? " << std::flush;
+        std::cerr << std::endl << "u_L? " << std::flush;
         std::cin >> uL;
-        std::cerr << "u_R? " << std::flush;
+        std::cerr << std::endl << "u_R? " << std::flush;
         std::cin >> uR;
         u2 = u_squared(uL,uR,xL,xR);
-        for(int ip = 0; ip < Npos; ++ip)
-      if(u2(xL+ip*dx) > u2_max)
-        u2_max = u2(xL+ip*dx);
         break;
       case 2:
-        std::cerr << "hOcean? " << std::endl;
+        std::cerr << std::endl << "hOcean? " << std::endl;
         std::cin >> hocean;
-        std::cerr << "xOcean? " << std::endl;
+        std::cerr << std::endl << "xOcean? " << std::endl;
         std::cin >> xocean;
-        std::cerr << "hPlage? " << std::endl;
+        std::cerr << std::endl << "hPlage? " << std::endl;
         std::cin >> hplage;
         u2 = u_squared(xL, xR, hocean, xocean, hplage);
-        for(int ip = 0; ip < Npos; ++ip)
-      if(u2(xL+ip*dx) > u2_max)
-        u2_max = u2(xL+ip*dx);
         break;
      }
-
-     std::cout << "u2_max="<<u2_max <<std::endl;
 }
 
-void contexte_temporelle(double& dt, double& tfinal, double const& dx, double const& u2_max)
+void contexte_temporelle(double& tfinal, double& CFL)
 {
-    double CFL;
-    std::cerr << "max CFL coefficent? " << std::flush;
+    std::cerr << std::endl << "max CFL coefficent? " << std::flush;
     std::cin >> CFL;
 
-    dt = CFL * dx / sqrt(u2_max);
-
-    std::cout << "dt=" << dt << std::endl;
-    std::cerr << "tfinal? " << std::flush;
+    std::cerr << std::endl << "tfinal? " << std::flush;
     std::cin >> tfinal;
 }
 
 void contexte_bord(std::string bord, boundary_condition& bc, double& boundaryvalue, double& A, double& omega)
 {
     int n;
-    std::cerr << bord << " boundary condition (fixed = 0, free = 1, excited = 2, outgoing = 3)? " << std::flush;
+    std::cerr << std::endl << bord << " boundary condition (fixed = 0, free = 1, excited = 2, outgoing = 3)? " << std::flush;
     std::cin >> n;
 
     switch(n)
@@ -115,38 +102,106 @@ void contexte_bord(std::string bord, boundary_condition& bc, double& boundaryval
         break;
 
       default:
-        std::cerr << "no valid " << bord << " boundary condition!";
+        std::cerr << std::endl << "no valid " << bord << " boundary condition!";
         throw "no valid boundary condition!";
       }
 
   if(bc == fixed)
   {
-    std::cerr << "for fixed left boundary condition: value of f at the left boundary? " << std::flush;
+    std::cerr << std::endl << "for fixed left boundary condition: value of f at the left boundary? " << std::flush;
     std::cin >> boundaryvalue;
   }
 
   if(bc == excited)
     {
-      std::cerr << "Amplitude of the excitation? " << std::flush;
+      std::cerr << std::endl << "Amplitude of the excitation? " << std::flush;
       std::cin >> A;
-      std::cerr << "Angular frequency omega of the excitation? " << std::flush;
+      std::cerr << std::endl << "Angular frequency omega of the excitation? " << std::flush;
       std::cin >> omega;
     }
 }
 
-void contexte_scan_frequence(int& nscan, double& omega_stop)
+void contexte_mode(int& nscan, double& omega_stop, int& Ninter_stop, double& CFL_stop, mode& choix)
 {
-    std::cerr << "number of frequencies to scan? " << std::flush;
-    std::cin >> nscan;
+    int n;
+    std::cerr << std::endl << " mode (unique = 0, scan frequence = 1, convergence CFL = 2, convergence dx = 3)? " << std::flush;
+    std::cin >> n;
 
-    if (nscan > 1) // do a frequency scan only if you are interested in more than 1 frequency
-      {
-        std::cerr << "scan from omega-start=omega to omega-end=? " << std::flush;
-        std::cin >> omega_stop;
-      }
+    switch(n)
+    {
+      case 0:
+        choix = unique;
+        break;
+
+      case 1:
+        choix = frequence;
+        break;
+
+      case 2:
+        choix = convergence_CFL;
+        break;
+
+      case 3:
+        choix = convergence_Ninter;
+        break;
+
+      default:
+        std::cerr << std::endl << "no valid mode";
+        throw "no valid mode";
+    }
+
+    if(choix == frequence)
+    {
+        std::cerr << std::endl << "number of frequencies to scan? " << std::flush;
+        std::cin >> nscan;
+
+        if (nscan > 1) // do a frequency scan only if you are interested in more than 1 frequency
+        {
+            std::cerr << std::endl << "scan from omega-start=omega to omega-end=? " << std::flush;
+            std::cin >> omega_stop;
+        }
+        else
+        {
+            nscan = 1;
+            choix = unique;
+        }
+    }
+    else if(choix == convergence_CFL)
+    {
+        std::cerr << std::endl << "number of CFL to scan? " << std::flush;
+        std::cin >> nscan;
+
+        if (nscan > 1)
+        {
+            std::cerr << std::endl << "scan from CFL-start=CFL to CFL-end=? " << std::flush;
+            std::cin >> CFL_stop;
+        }
+        else
+        {
+            nscan = 1;
+            choix = unique;
+        }
+    }
+    else if(choix == convergence_Ninter)
+    {
+        std::cerr << std::endl << "number of Ninter to scan? " << std::flush;
+        std::cin >> nscan;
+
+        if (nscan > 1)
+        {
+            std::cerr << std::endl << "scan from Ninter-start=Ninter to Ninter-end=? " << std::flush;
+            std::cin >> Ninter_stop;
+        }
+        else
+        {
+            nscan = 1;
+            choix = unique;
+        }
+    }
     else
     {
         nscan = 1;
+        choix = unique;
     }
 }
 
